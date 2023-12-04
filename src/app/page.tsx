921db22311feddd1917fb3f6e03b5fcc28e54b1a
Xiangyi Li <venusli370306@gmail.com>
2023-12-04 01:47:20 -0800

    chore: update icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,26 @@
-'use client'
+"use client";
 
-import {ChangeEvent, useState} from 'react'
-import type {githubTypes} from '@opensdks/sdk-github'
-import CommitsList from '@/components/CommitsList'
-import {Button} from '@/components/ui/button'
+import { ChangeEvent, useState } from "react";
+import type { githubTypes } from "@opensdks/sdk-github";
+import CommitsList from "@/components/CommitsList";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from '@/components/ui/card'
-import {Input} from '@/components/ui/input'
-import {Label} from '@/components/ui/label'
-import {fetchCommits, summarizeCommits} from './actions'
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { fetchCommits, summarizeCommits } from "./actions";
 
-type Commit = githubTypes['components']['schemas']['commit']
+type Commit = githubTypes["components"]["schemas"]["commit"];
 
 export default function Component() {
-  const [commits, setCommits] = useState<Commit[]>([])
-  const [prLink, setPrLink] = useState('')
-  const [summary, setSummary] = useState<string | null>('')
+  const [commits, setCommits] = useState<Commit[]>([]);
+  const [prLink, setPrLink] = useState("");
+  const [summary, setSummary] = useState<string | null>("");
 
   return (
     <div className="flex flex-col w-md min-h-screen p-10">
@@ -32,15 +32,16 @@ export default function Component() {
         <form
           className="space-y-4"
           action={async (formData: FormData) => {
-            const prLink = formData.get('pr-link') as string
+            const prLink = formData.get("pr-link") as string;
             if (!prLink) {
-              return
+              return;
             }
-            const fetchedCommits = await fetchCommits(prLink)
-            setCommits(fetchedCommits)
-            const summary = await summarizeCommits(fetchedCommits)
-            setSummary(summary)
-          }}>
+            const fetchedCommits = await fetchCommits(prLink);
+            setCommits(fetchedCommits);
+            const summary = await summarizeCommits(fetchedCommits);
+            setSummary(summary);
+          }}
+        >
           <div className="space-y-2">
             <Label htmlFor="pr-link">Pull Request Link</Label>
             <Input
@@ -48,14 +49,15 @@ export default function Component() {
               name="pr-link"
               placeholder="https://github.com/user/repo/pull/123"
               onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                setPrLink(e.target.value)
+                setPrLink(e.target.value);
               }}
               required
             />
           </div>
           <Button
             className="w-full bg-primary text-primary-foreground hover:bg-primary/90"
-            type="submit">
+            type="submit"
+          >
             Analyze
           </Button>
         </form>
@@ -69,7 +71,7 @@ export default function Component() {
             </CardHeader>
             <CardContent>
               <p className="text-gray-500 dark:text-gray-400 text-sm break-words">
-                {summary || 'OpenAI generated commit summary goes here...'}
+                {summary || "OpenAI generated commit summary goes here..."}
               </p>
             </CardContent>
           </div>
@@ -87,5 +89,5 @@ export default function Component() {
         </Card>
       </div>
     </div>
-  )
+  );
 }
